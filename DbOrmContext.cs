21e93bec25106b9504a5b3102a5746bac5c551fa
mrunalmandhare26 <108930269+mrunalmandhare26@users.noreply.github.com>
2022-07-08 15:45:13 +0530

    added file

---
+++ DbOrmContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+using MVC_Entity_Framework.Models;
+
+
+namespace MVC_Entity_Framework.DAL
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DbOrmContext:DbContext
+    {
+        public DbSet<Person> persons { get; set; }
+
+        public DbOrmContext() : base("WebAppCon")
+        {
+
+        }
+    }
+}
